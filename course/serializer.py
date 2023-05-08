0f9fd73d08416b930f725786cc6a9036c6cc775a
“@ruiyi0118” <906063940@qq.com>
2023-05-08 17:15:05 +0800

    带URL的序列化

--- course/serializer.py
+++ course/serializer.py
@@ -10,12 +10,19 @@ class UserSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class CourseSerializer(serializers.ModelSerializer):
+# class CourseSerializer(serializers.ModelSerializer):
+#
+#     # 如果想得到讲师的名字，将用户的名称序列化
+#     teacher = serializers.ReadOnlyField(source='teacher.username')  # 外键字段 只读
+#     class Meta:
+#         model = Course
+#         # exclude = ('id', )  # 排除某些元素
+#         # fields = ("name", "introduction")
+#         fields = '__all__'
 
-    # 如果想得到讲师的名字，将用户的名称序列化
-    teacher = serializers.ReadOnlyField(source='teacher.username')  # 外键字段 只读
+class CourseSerializer(serializers.HyperlinkedModelSerializer):
+    teacher = serializers.ReadOnlyField(source='teacher.username')
+
     class Meta:
         model = Course
-        # exclude = ('id', )  # 排除某些元素
-        # fields = ("name", "introduction")
-        fields = '__all__'
+        fields = ('id', 'url', 'name', 'introduction', 'teacher', 'price', 'created_at', 'updated_at')

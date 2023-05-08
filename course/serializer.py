fce097078b529617c44b6807aec0615a8c947735
“@ruiyi0118” <906063940@qq.com>
2023-05-08 17:10:40 +0800

    序列化模型类

---
+++ course/serializer.py
@@ -0,0 +1,21 @@
+from django import forms
+from rest_framework import serializers
+from .models import Course
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class CourseSerializer(serializers.ModelSerializer):
+
+    # 如果想得到讲师的名字，将用户的名称序列化
+    teacher = serializers.ReadOnlyField(source='teacher.username')  # 外键字段 只读
+    class Meta:
+        model = Course
+        # exclude = ('id', )  # 排除某些元素
+        # fields = ("name", "introduction")
+        fields = '__all__'
